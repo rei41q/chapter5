58af884843a1ec938585a88612ff33f1768b7ef3
Rei Greiq <greiqrei@gmail.com>
2022-06-29 08:57:16 +0800

    update all

--- sayHai.js
+++ sayHai.js
@@ -11,3 +11,13 @@ const os = require("os");
 const readingLocation = os.homedir();
 
 console.log(readingLocation);
+
+const calculator = require("./utils/calculator");
+
+const tambahkan = calculator.addition(2,3);
+
+const kurangkan = calculator.substrack(5,2);
+
+console.log("2 + 3 = ",tambahkan);
+
+console.log("5 - 2 = ",kurangkan);
---
+++ utils/calculator.js
@@ -0,0 +1,13 @@
+function additonal(x, y){ //function2
+    return x+y;
+}
+
+const addition = (x,y) =>{ // function1
+    return x+y;
+}
+
+const substrack = (x, y) =>{
+    return x - y;
+}
+
+module.exports = {addition, substrack};
